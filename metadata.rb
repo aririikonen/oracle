6067f2a28903156f401f2f01f4a6db6814c830d4
Ari Riikonen <ari.riikonen@gmail.com>
2014-07-10 10:19:08 +0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'Apache 2.0'
 description      'Installs/Configures Oracle rdbms and client on CentOS 6.5'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.2'
+supports         'redhat'
+supports         'centos'
